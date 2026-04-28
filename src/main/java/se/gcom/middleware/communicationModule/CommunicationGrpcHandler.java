e4b985928cffc78df769dfba53d3d945b718c5b9
Jonatan Westling <jonatanwestling4@gmail.com>
2026-04-28 14:42:58 +0200

    refactor: moved files from v.1

---
+++ src/main/java/se/gcom/middleware/communicationModule/CommunicationGrpcHandler.java
@@ -0,0 +1,18 @@
+package se.gcom.middleware.communicationModule;
+
+import io.grpc.stub.StreamObserver;
+
+public class CommunicationGrpcHandler extends CommunicationServiceGrpc.CommunicationServiceImplBase {
+    @Override
+    public void sendMessage(ChatMessage request, StreamObserver<Ack> responseObserver) {
+
+        System.out.println("Received: " + request.getPayload());
+        System.out.println(request.getSenderId());
+        System.out.println(request.getReceiverId());
+        System.out.println(request.getMessageId());
+
+        Ack ack = Ack.newBuilder().setSuccess(true).build();
+        responseObserver.onNext(ack);
+        responseObserver.onCompleted();
+    }
+}
