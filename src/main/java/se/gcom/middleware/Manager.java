9103c226eeb65a0395e08154bfd3951b87b5c7c4
Jonatan Westling <jonatanwestling4@gmail.com>
2026-04-29 09:41:21 +0200

    send function

--- src/main/java/se/gcom/middleware/Manager.java
+++ src/main/java/se/gcom/middleware/Manager.java
@@ -2,14 +2,23 @@ package se.gcom.middleware;
 
 import se.gcom.app.controller.ChatController;
 import se.gcom.app.controller.DebugController;
+import se.gcom.middleware.communicationModule.CommunicationGrpcHandler;
+import se.gcom.middleware.communicationModule.CommunicationGrpcSender;
 
 public class Manager {
 
     ChatController chatController;
     DebugController debugController;
+    CommunicationGrpcSender communicationGrpcSender;
+    CommunicationGrpcHandler communicationGrpcHandler;
 
     public Manager(ChatController chatController, DebugController debugController) {
         this.chatController = chatController;
         this.debugController = debugController;
     }
+
+    public void sendMessage(String sender, String message){
+
+    }
+
 }
