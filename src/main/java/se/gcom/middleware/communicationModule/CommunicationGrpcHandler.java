48c3f620e6ea68954a1354d9f48a649b731b8120
Jonatan Westling <jonatanwestling4@gmail.com>
2026-05-05 13:05:29 +0200

    fix: handle vector clock ack

--- src/main/java/se/gcom/middleware/communicationModule/CommunicationGrpcHandler.java
+++ src/main/java/se/gcom/middleware/communicationModule/CommunicationGrpcHandler.java
@@ -3,6 +3,8 @@ package se.gcom.middleware.communicationModule;
 import io.grpc.stub.StreamObserver;
 import se.gcom.middleware.Manager;
 
+import java.util.Map;
+
 public class CommunicationGrpcHandler extends CommunicationServiceGrpc.CommunicationServiceImplBase {
 
     private final Manager manager;
@@ -29,9 +31,27 @@ public class CommunicationGrpcHandler extends CommunicationServiceGrpc.Communica
                 GroupMembership groupMembership = msg.getGroupMembership();
                 // here the message can be received right away
                 manager.deliverIncomingMessage(groupMembership);
+
+                if (groupMembership.getJoining()){
+                    if (manager.orderingIsCausal(groupMembership.getGroupId())){
+                        // the group is causally ordered so we need to append the vector clock to the ack
+                        Map<String, Integer> currentVC = manager.getCurrentVectorClock(groupMembership.getGroupId());
+                        // joining message, respond ack with vector clock
+                        MembershipAck membershipAck = MembershipAck.newBuilder().putAllVectorClock(currentVC).build();
+                        // pack into the ack
+                        Ack ack = Ack.newBuilder()
+                                .setSuccess(true)
+                                .setMembership(membershipAck)
+                                .build();
+
+                        responseObserver.onNext(ack);
+                        responseObserver.onCompleted();
+                        return;
+                    }
+                }
                 break;
         }
-
+        // Normal ack
         Ack ack = Ack.newBuilder().setSuccess(true).build();
         responseObserver.onNext(ack);
         responseObserver.onCompleted();
