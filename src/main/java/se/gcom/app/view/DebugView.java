8c1b4644dc47bc8d7f3a3ad690ca56aa40b69bf8
Jonatan Westling <jonatanwestling4@gmail.com>
2026-05-11 09:28:57 +0200

    fix: removed import and usage of classes that does not exist

--- src/main/java/se/gcom/app/view/DebugView.java
+++ src/main/java/se/gcom/app/view/DebugView.java
@@ -8,12 +8,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import se.gcom.app.controller.DebugController;
 import se.gcom.app.view.debug.DebugConsole;
-import se.gcom.app.view.debug.DebugBuffers;
-import se.gcom.app.view.debug.DebugNetworkTrace;
-import se.gcom.app.view.debug.DebugOrdering;
 import se.gcom.app.view.debug.DebugOverview;
-import se.gcom.app.view.debug.DebugPerformance;
-import se.gcom.app.view.debug.DebugTestControls;
 
 public class DebugView extends BorderPane {
 
@@ -54,18 +49,6 @@ public class DebugView extends BorderPane {
         sidebar.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if ("Overview".equals(newValue)) {
                 setCenter(new DebugOverview());
-            } else if ("Network Trace".equals(newValue)) {
-                setCenter(new DebugNetworkTrace());
-            } else if ("Ordering".equals(newValue)) {
-                setCenter(new DebugOrdering());
-            } else if ("Buffers".equals(newValue)) {
-                setCenter(new DebugBuffers());
-            } else if ("Performance".equals(newValue)) {
-                setCenter(new DebugPerformance());
-            } else if ("Test Controls".equals(newValue)) {
-                setCenter(new DebugTestControls());
-            } else if ("Console".equals(newValue)) {
-                setCenter(new DebugConsole(controller));
             }
         });
     }
