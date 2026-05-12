825a7c9b1eacfc3959a1f8f448340e364c27c82d
Algot Granér <algot.graner@gmail.com>
2026-05-12 10:51:50 +0200

    Add ip-address to debug window name

--- src/main/java/se/gcom/app/view/ChatView.java
+++ src/main/java/se/gcom/app/view/ChatView.java
@@ -523,7 +523,7 @@ public class ChatView {
         DebugView debugView = new DebugView(debugController);
 
         Stage debugStage = new Stage();
-        debugStage.setTitle("Debug View");
+        debugStage.setTitle("Debug View -- " + chatController.getAddress());
         debugStage.setScene(new Scene(debugView, 1200, 800));
         debugStage.show();
     }
